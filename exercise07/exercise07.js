2921052690e7ce909797c408f82ec0595283d4cd
Henrique Luz <riqueluz@gmail.com>
2015-02-12 18:41:51 -0300

    exercise 07: controls

---
+++ exercise07/exercise07.js
@@ -0,0 +1,47 @@
+function render() {
+	renderer.setSize(width + 200, height + 100);
+	document.body.appendChild(renderer.domElement);
+	
+	function animate() {
+		requestAnimationFrame(animate);
+		renderer.render(scene, camera);
+		controls.update();
+	}
+	
+	var controls = new THREE.OrbitControls(camera);
+	var planeGeometry = new THREE.PlaneBufferGeometry(80, 80);
+	var planeMaterial = new THREE.MeshLambertMaterial({color: GREEN});
+	var plane = new THREE.Mesh(planeGeometry, planeMaterial);
+	
+	var sphereGeometry = new THREE.SphereGeometry(4, 8, 8);
+	var sphereMaterial = new THREE.MeshPhongMaterial({
+		color : RED
+	});
+	var sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+	var light = new THREE.SpotLight(WHITE);
+	var anotherLight = new THREE.SpotLight(SILVER, 2);
+
+	sphere.castShadow = true;
+	sphere.position.set(0, 10, -15);
+	
+	plane.receiveShadow = true;
+	plane.position.set(0, 0, -30);
+	
+	light.shadowDarkness = 0.5;
+	light.target = sphere;
+	light.position.set(0, -10, 90);
+	light.castShadow =  true;
+	light.shadowCameraVisible = true;
+	light.shadowCameraFar = 150;
+	light.shadowCameraFov = 30;
+	anotherLight.position.set(20, -60, -60);
+
+	scene.add(light);
+	scene.add(sphere);
+	scene.add(plane);
+	//scene.add(anotherLight);
+
+	camera.position.z = 100;
+	renderer.render(scene, camera);
+	animate();
+}
